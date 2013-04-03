bd555e5c7a9acafdfe90851b1118fd547503dca0
lisacf_ericxu <lisacf@gmail.com>
2013-04-02 22:11:02 -0700

    add validation to Response so that a user cannot answer to his own poll

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,12 +3,19 @@ class Response < ActiveRecord::Base
 
   validates :user, :choice, :presence => true
   validate :validate_response
+  validate :creator_can_not_add_to_own_poll
   belongs_to :user
   belongs_to :choice
 
   def validate_response
     if Response.where(:user_id => user_id).pluck('choice_id').include?(choice_id)
       errors[:base]<< "choice already selected by user"
+    end
+  end
+
+  def creator_can_not_add_to_own_poll
+    if self.choice.question.poll.user.id == self.user_id
+      errors[:response] << "can not be added to user's own poll"
     end
   end
 end
